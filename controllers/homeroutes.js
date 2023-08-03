1742bceddf8b9a76a49983082358f61d2ee71f76
Alex <aminyakov94@gmail.com>
2023-08-03 17:45:25 -0500

    added home routes

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -14,3 +14,44 @@ router.get("/",async (req,res)=>{
 
     }
 })
+router.get("/posts/:id",async (req,res)=>{
+    try{
+        const data=await Post.findByPk(req.params.id,{
+            include:[
+                User,
+                {
+                    model:Comment,
+                    include:[User],
+                }
+            ]
+        })
+        if(data){
+            const post=data.get({plain:true})
+            res.render("onepost",{post})
+
+        }    else{
+            res.status(404).end()
+        }
+    }catch(err){
+        res.status(500).json(err)
+
+    }
+})
+
+router.get("/login",(req,res)=>{
+    if(req.session.loggedIn){
+        res.redirect("/")
+        return
+    }
+    res.render("login")
+})
+
+router.get("/signup",(req,res)=>{
+    if(req.session.loggedIn){
+        res.redirect("/")
+        return
+    }
+    res.render("signup")
+})
+
+module.exports=router
