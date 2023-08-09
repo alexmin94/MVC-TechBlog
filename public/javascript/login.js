61b8d365d2903dccf6d7239c961b23241ba95f4c
Alex <aminyakov94@gmail.com>
2023-08-09 11:28:59 -0500

    added login function

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,1 +1,21 @@
-const login
+const login= async function(event){
+    event.preventDefault()
+    var usernameElement=document.querySelector("#username-input-login")
+    var passwordElement=document.querySelector("#passoword-input-login")
+    const response = await fetch('/api/user/login', {
+        method: 'POST',
+        body: JSON.stringify({
+          username: usernameElement.value,
+          password: passwordElement.value,
+        }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert('Failed to login');
+      }
+}
+document
+  .querySelector('#login-form')
+  .addEventListener('submit', loginFormHandler);
